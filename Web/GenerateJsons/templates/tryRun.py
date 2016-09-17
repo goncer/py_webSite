0251d79e8c3e356947ab48876f61c2b3b0bc5f55
gonzo <gonza.cerro@gmail.com>
2016-09-17 11:01:37 +0200

    asd

--- Web/GenerateJsons/templates/tryRun.py
+++ Web/GenerateJsons/templates/tryRun.py
@@ -7,9 +7,11 @@ url = 'http://api-loop-publication.frontiersin.org/api/v1/info/ManuallyAddedPubl
 data = ''
 response = requests.get(url, data=data)
 jData = json.loads(response.content)
+typeObject = jData["apis"][0]["operations"][0]["parameters"][1]["dataType"]
+
 models = jData["models"]
 for model in models:
-    for propertie in model["properties"]:
+    for property in model["properties"]:
         print propertie
     print model
 props = models[0]["properties"]
