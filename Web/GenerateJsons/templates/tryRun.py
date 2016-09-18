657c2f1247d6788f2135e7dd743163c596e8b011
gonzo <gonza.cerro@gmail.com>
2016-09-18 13:12:13 +0200

    asdasdasd

--- Web/GenerateJsons/templates/tryRun.py
+++ Web/GenerateJsons/templates/tryRun.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 import random
 import requests
 from requests.auth import HTTPDigestAuth
@@ -14,7 +16,8 @@ def getPayload(modelObject):
             return str(random.randint(0, 100))
         elif (modelObject == "long"):
             return str(random.randint(10000, 1000000) )
-    print "ERROR" + modelObject
+
+    print ("ERROR" + modelObject , "\n")
     return "ERROR";
 
 
@@ -28,22 +31,32 @@ def generateParameter(key,typeObject, models):
     if(type(typeObject) is dict):
         if (typeObject["type"] == "string" or typeObject["type"] == "int"  or typeObject["type"] == "long" ):
             concatenated = "\"" + key + ":\""  + getPayload(typeObject["type"])
-            print concatenated
+            print (concatenated ,end = "\n" )
             return  concatenated;
-    else:
-        if typeObject in models.keys():
-            m_model = models.get(typeObject)
+        if (typeObject["type"] == "Array" ):
+            #str("[" + getItem + "]")
+            name=typeObject["items"].values()[0]
+            m_model = models.get(name)
+            return "["  + recursive_getModelEleemntValues(models.get(name), m_payload, models) + "]"
 
-            for m_key,m_value in m_model["properties"].iteritems():
-                m_payload += generateParameter(m_key,m_value,models)
-                print m_payload
-            m_payload += generateParameterForModel(typeObject, models)
-            return m_payload;
-        else:
-            print "something wrong happens"
-    print "something wrong happens"
+    if (type(typeObject) is dict):
+        m_model = models.get(typeObject["type"])
+    elif(type(typeObject) is unicode):
+        m_model = models.get(typeObject)
+    if(m_model != None):
+        return recursive_getModelEleemntValues(m_model, m_payload, models)
+    else:
+        print ("something wrong happens" )
+    print ("something wrong happens")
     return "";
 
+
+def recursive_getModelEleemntValues(m_model, m_payload, models):
+    for m_key, m_value in m_model["properties"].iteritems():
+        m_payload += generateParameter(m_key, m_value, models)
+        print(m_payload, end="\n")
+    # m_payload += generateParameterForModel(m_key,typeObject, models)
+    return m_payload;
 
 
 url = 'http://api-loop-publication.frontiersin.org/api/v1/info/ManuallyAddedPublications'
@@ -55,13 +68,6 @@ typeObject = jData["apis"][0]["operations"][0]["parameters"][1]["dataType"]
 nameObject = jData["apis"][0]["operations"][0]["parameters"][1]["name"]
 parameterValue = generateParameter("asd",typeObject,jData["models"])
 models = jData["models"]
-for model in models:
-    for property in model["properties"]:
-        print property
-    print model
-props = models[0]["properties"]
-a = props[1]
-k = 0
 
 
 
